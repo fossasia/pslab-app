225f42b7a84614c2878287f0a4f7290120fe207a
Vidhi Jain <vjvidhijain2004@gmail.com>
2025-05-17 23:06:16 +0530

    feat: added Connect Device screen

--- lib/view/connect_device_screen.dart
+++ lib/view/connect_device_screen.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pslab/view/widgets/main_scaffold_widget.dart';
 
 class ConnectDeviceScreen extends StatefulWidget {
   const ConnectDeviceScreen({super.key});
+  final String iconUsbDisconnected =
+      'assets/icons/icons_usb_disconnected_100.png';
 
   @override
   State<StatefulWidget> createState() => _HomeScreenState();
@@ -10,7 +13,160 @@ class ConnectDeviceScreen extends StatefulWidget {
 class _HomeScreenState extends State<ConnectDeviceScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MainScaffold(
+      index: 2,
+      title: 'Connect Device',
+      body: SafeArea(
+        child: ScrollConfiguration(
+          behavior: const ScrollBehavior(),
+          child: ListView(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Image.asset(
+                  widget.iconUsbDisconnected,
+                  width: 80,
+                  height: 80,
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 20, bottom: 60),
+                  child: Text(
+                    'No USB Device Found',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blueGrey[600],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 40, right: 40, bottom: 20),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Steps to connect the PSLab Device',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      '1. Connect a micro USB(Mini B) to PSLab',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      '2. Connect the other end of the micro USB cable to a OTG',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      '3. Connect the OTG to the phone',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'Connect using Bluetooth or Wi-Fi',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                        backgroundColor: const Color(0xFFD32F2F),
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'BLUETOOTH',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                        backgroundColor: const Color(0xFFD32F2F),
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'WIFI',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 30, left: 120, right: 120),
+                child: const Divider(color: Colors.grey, height: 1),
+              ),
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.all(10),
+                  child: GestureDetector(
+                    onTap: () {
+                      /**/
+                    },
+                    child: const Text(
+                      'What is PSLab Device?',
+                      style: TextStyle(
+                        color: Color(0xFFD32F2F),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
