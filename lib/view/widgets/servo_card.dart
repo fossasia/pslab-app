924e649d1a49ab53d5b983a50c01189be173b5f3
Rahul Das <rdass87871@gmail.com>
2025-08-07 21:51:12 +0530

    changes

--- lib/view/widgets/servo_card.dart
+++ lib/view/widgets/servo_card.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:pslab/view/widgets/servo_slider.dart';
-import '../../l10n/app_localizations.dart';
-import '../../providers/locator.dart';
+import 'package:pslab/theme/colors.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
 import '../../providers/robotic_arm_state_provider.dart';
 
-class ServoCard extends StatefulWidget {
+class ServoCard extends StatelessWidget {
   final double value;
   final Function(double) onChanged;
   final VoidCallback onTap;
@@ -24,25 +24,11 @@ class ServoCard extends StatefulWidget {
   });
 
   @override
-  State<ServoCard> createState() => _ServoCardState();
-}
-
-class _ServoCardState extends State<ServoCard> {
-  late double currentValue;
-
-  @override
-  void initState() {
-    super.initState();
-    currentValue = widget.value;
-  }
-
-  @override
   Widget build(BuildContext context) {
     final provider = Provider.of<RoboticArmStateProvider>(context);
-    final sliderSize = provider.maxAngle == 180
-        ? widget.cardHeight * 0.95
-        : widget.cardHeight * 0.85;
-    AppLocalizations appLocalizations = getIt.get<AppLocalizations>();
+    final sliderSize =
+    provider.maxAngle == 180 ? cardHeight * 0.95 : cardHeight * 0.75;
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
@@ -55,7 +41,7 @@ class _ServoCardState extends State<ServoCard> {
             top: 0,
             left: 2,
             child: Text(
-              widget.label,
+              label,
               style: const TextStyle(
                 fontSize: 10,
                 fontWeight: FontWeight.w500,
@@ -68,19 +54,19 @@ class _ServoCardState extends State<ServoCard> {
             right: 0,
             child: Draggable<Map<String, dynamic>>(
               data: {
-                'servoId': widget.servoId,
-                'degree': provider.maxAngle == 180
-                    ? widget.value.clamp(0, 180).round()
-                    : widget.value.clamp(0, 360).round(),
+                'servoId': servoId,
+                'degree': value,
               },
               feedback: Material(
                 color: Colors.transparent,
                 child: Container(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-                  decoration: const BoxDecoration(color: Colors.black),
+                  const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
                   child: Text(
-                    '${provider.maxAngle == 180 ? widget.value.clamp(0, 180).round() : widget.value.clamp(0, 360).round()} ',
+                    '${value.floor()} °',
                     style: const TextStyle(
                       color: Colors.white,
                       fontSize: 24,
@@ -90,52 +76,52 @@ class _ServoCardState extends State<ServoCard> {
                 ),
               ),
               childWhenDragging:
-                  const Icon(Icons.drag_handle, size: 24, color: Colors.grey),
+              const Icon(Icons.drag_handle, size: 24, color: Colors.grey),
               child:
-                  const Icon(Icons.drag_handle, size: 24, color: Colors.grey),
+              const Icon(Icons.drag_handle, size: 24, color: Colors.grey),
             ),
           ),
           Positioned.fill(
             top: provider.maxAngle == 180 ? 25 : 0,
             child: GestureDetector(
-              onTap: widget.onTap,
+              onTap: onTap,
               child: Center(
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    ServoSlider(
-                        progress: widget.value,
-                        maxProgress: provider.maxAngle.toDouble(),
-                        size: sliderSize,
-                        trackWidth: 5,
-                        thumbRadius: 9,
-                        sweepAngle: provider.maxAngle.toDouble(),
-                        startAngle:
-                            provider.maxAngle.toDouble() == 180 ? 180 : 270,
-                        clockwise: true,
-                        trackColor: Colors.grey.shade300,
-                        progressColor: Colors.red,
-                        thumbColor: Colors.cyan,
-                        onChanged: widget.onChanged),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          ' ${provider.maxAngle == 180 ? widget.value.clamp(0, 180).round() : widget.value.clamp(0, 360).round()}${appLocalizations.degreeSymbol}',
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.black,
-                            fontWeight: FontWeight.normal,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                        Text(
-                          appLocalizations.tapToEdit,
-                          style: TextStyle(fontSize: 7, color: Colors.grey),
-                        ),
-                      ],
+                child: SleekCircularSlider(
+                  initialValue: value.clamp(0, provider.maxAngle.toDouble()),
+                  key: ValueKey(provider.maxAngle),
+                  min: 0,
+                  max: provider.maxAngle.toDouble(),
+                  onChange: onChanged,
+                  appearance: CircularSliderAppearance(
+                    size: sliderSize,
+                    startAngle: provider.maxAngle == 180 ? 180 : 270,
+                    angleRange: provider.maxAngle.toDouble(),
+                    customWidths: CustomSliderWidths(
+                      trackWidth: 8,
+                      progressBarWidth: 8,
+                      handlerSize: 14,
                     ),
-                  ],
+                    customColors: CustomSliderColors(
+                      trackColor: Colors.grey.shade300,
+                      progressBarColor: primaryRed,
+                      dotColor: Colors.black38,
+                    ),
+                    infoProperties: InfoProperties(
+                      mainLabelStyle: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 10,
+                            color: Colors.black.withAlpha((0.3 * 255).round()),
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      modifier: (val) => '${val.round()}°',
+                    ),
+                  ),
                 ),
               ),
             ),
