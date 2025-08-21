5fc2d8726ea6270232c6d0d2de49f245fad127f9
Anashuman Singh <anash.cheema@gmail.com>
2025-08-21 11:31:02 +0530

    fix: multimeter knob UI

--- lib/view/widgets/multimeter_knob.dart
+++ lib/view/widgets/multimeter_knob.dart
@@ -12,7 +12,7 @@ class InnerDialPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = min(size.width / 2, size.height / 2) * 0.7;
+    final radius = 105.r;
 
     final paint = Paint()
       ..color = multimeterBorderBlack
@@ -32,7 +32,7 @@ class InnerDialFillPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = min(size.width / 2, size.height / 2) * 0.7;
+    final radius = 105.r;
 
     final fillPaint = Paint()
       ..color = innerDialFillColor
@@ -59,13 +59,13 @@ class InnerPointerPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = min(size.width / 2, size.height / 2) * 0.5;
+    final radius = 105.r;
 
     final pointerAngle = -pi / 2 + (2 * pi * (value / max));
 
     final pointerPaint = Paint()
       ..color = color
-      ..strokeCap = StrokeCap.square
+      ..strokeCap = StrokeCap.butt
       ..strokeWidth = 30;
 
     final pointerStart = Offset(
@@ -82,12 +82,12 @@ class InnerPointerPainter extends CustomPainter {
       ..strokeCap = StrokeCap.square
       ..strokeWidth = 10;
     final pointerStartInner = Offset(
-      center.dx + radius * 1.1 * cos(pointerAngle),
-      center.dy + radius * 1.1 * sin(pointerAngle),
+      center.dx + radius * 1.0 * cos(pointerAngle),
+      center.dy + radius * 1.0 * sin(pointerAngle),
     );
     final pointerEndInner = Offset(
-      center.dx + radius * 0.9 * cos(pointerAngle),
-      center.dy + radius * 0.9 * sin(pointerAngle),
+      center.dx + radius * 0.77 * cos(pointerAngle),
+      center.dy + radius * 0.77 * sin(pointerAngle),
     );
     canvas.drawLine(pointerStart, pointerEnd, pointerPaint);
     canvas.drawLine(pointerStartInner, pointerEndInner, pointerPaintInner);
@@ -271,14 +271,8 @@ class _MultimeterKnobState extends State<MultimeterKnob> {
                   color: pointerColor,
                 ),
                 child: SizedBox(
-                  width: constraints.maxWidth < 600 ||
-                          constraints.maxWidth > constraints.maxHeight
-                      ? 360.w
-                      : 385.w,
-                  height: constraints.maxWidth < 600 ||
-                          constraints.maxWidth > constraints.maxHeight
-                      ? 360.h
-                      : 385.h,
+                  width: 300.w,
+                  height: 300.h,
                 ),
               ),
             ),
