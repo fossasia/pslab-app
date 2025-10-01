789d010f6740d555c910ca9e20d9a3f5e25da840
Anashuman Singh <anash.cheema@gmail.com>
2025-10-01 21:23:21 +0530

    feat: finalize Logic Analyzer screen

---
+++ lib/models/logic_analyzer_config.dart
@@ -0,0 +1,27 @@
+class LogicAnalyzerConfig {
+  final bool includeLocationData;
+
+  const LogicAnalyzerConfig({
+    this.includeLocationData = true,
+  });
+
+  LogicAnalyzerConfig copyWith({
+    bool? includeLocationData,
+  }) {
+    return LogicAnalyzerConfig(
+      includeLocationData: includeLocationData ?? this.includeLocationData,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'includeLocationData': includeLocationData,
+    };
+  }
+
+  factory LogicAnalyzerConfig.fromJson(Map<String, dynamic> json) {
+    return LogicAnalyzerConfig(
+      includeLocationData: json['includeLocationData'] ?? true,
+    );
+  }
+}
