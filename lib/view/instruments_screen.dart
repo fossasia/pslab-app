9c0d2d514b01decef23a780e374599055136c02a
Marc Nause <marc.nause@audioattack.de>
2025-12-14 23:51:20 +0100

    final changes

--- lib/view/instruments_screen.dart
+++ lib/view/instruments_screen.dart
@@ -114,8 +114,8 @@ class _InstrumentsScreenState extends State<InstrumentsScreen> {
       _InstrumentData(appLocalizations.roboticArm,
           appLocalizations.roboticArmDesc, '/roboticArm'),
       // Instruments below are not yet implemented.
-      //_InstrumentData(appLocalizations.gasSensor, appLocalizations.gasSensorDesc, 'gassensor'),
-      //_InstrumentData(appLocalizations.dustSensor, appLocalizations.dustSensorDesc, 'dustsensor'),
+      //_InstrumentData(appLocalizations.gasSensor, appLocalizations.gasSensorDesc, '/gassensor'),
+      //_InstrumentData(appLocalizations.dustSensor, appLocalizations.dustSensorDesc, '/dustsensor'),
       _InstrumentData(appLocalizations.soundMeter,
           appLocalizations.soundMeterDesc, '/soundmeter'),
     ];
