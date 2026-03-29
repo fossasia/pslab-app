e7b60350f15e8383490cbc55ad1567b0f0edaacb
SHEKIRFAN2007 <s.shekirfan993@gmail.com>
2026-03-29 22:18:09 +0530

    refactor: simplify sensor screens to use shared widget
    - Convert GasSensorScreen and DustSensorScreen to StatelessWidget
    - Use FeatureNotImplementedScreen reusable component
    - Remove ~100 lines of duplicated code per file
    - Maintain same UX with cleaner implementation

--- lib/view/gas_sensor_screen.dart
+++ lib/view/gas_sensor_screen.dart
@@ -1,116 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pslab/l10n/app_localizations.dart';
 import 'package:pslab/providers/locator.dart';
-import 'package:pslab/view/widgets/common_scaffold_widget.dart';
+import 'package:pslab/view/widgets/feature_not_implemented_screen.dart';
 
-class GasSensorScreen extends StatefulWidget {
+class GasSensorScreen extends StatelessWidget {
   const GasSensorScreen({super.key});
 
   @override
-  State<GasSensorScreen> createState() => _GasSensorScreenState();
-}
-
-class _GasSensorScreenState extends State<GasSensorScreen> {
-  AppLocalizations appLocalizations = getIt.get<AppLocalizations>();
-
-  @override
-  void initState() {
-    super.initState();
-    _setPortraitOrientation();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  }
-
-  @override
-  void didChangeDependencies() {
-    _setPortraitOrientation();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-    super.didChangeDependencies();
-  }
-
-  void _setPortraitOrientation() {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return CommonScaffold(
+    AppLocalizations appLocalizations = getIt.get<AppLocalizations>();
+    
+    return FeatureNotImplementedScreen(
       title: appLocalizations.gasSensor,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.air,
-                size: 120,
-                color: Colors.grey.shade400,
-              ),
-              const SizedBox(height: 32),
-              Text(
-                appLocalizations.gasSensor,
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                appLocalizations.screenNotImplemented,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color: Colors.grey.shade600,
-                    ),
-              ),
-              const SizedBox(height: 24),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.blue.shade50,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(color: Colors.blue.shade200),
-                ),
-                child: Column(
-                  children: [
-                    Icon(
-                      Icons.info_outline,
-                      color: Colors.blue.shade700,
-                      size: 32,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      appLocalizations.gasSensorDesc,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.blue.shade900,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.arrow_back),
-                label: Text(appLocalizations.close),
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      description: appLocalizations.gasSensorDesc,
+      icon: Icons.air,
+      accentColor: Colors.blue,
     );
   }
 }
