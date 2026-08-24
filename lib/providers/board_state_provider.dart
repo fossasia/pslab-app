efc556565bc7112c6b329c055156b901cc48133e
rahul31124 <rdass87871@gmail.com>
2026-08-24 16:19:56 +0530

    curly braces

--- lib/providers/board_state_provider.dart
+++ lib/providers/board_state_provider.dart
@@ -91,8 +91,9 @@ class BoardStateProvider extends ChangeNotifier {
   }
 
   Future<bool> _connectToDesktopDynamic() async {
-    if (ScienceLabCommon.communicationHandler is! PSLabCommunicationHandler)
+    if (ScienceLabCommon.communicationHandler is! PSLabCommunicationHandler) {
       return false;
+    }
 
     final comms =
         ScienceLabCommon.communicationHandler as PSLabCommunicationHandler;
