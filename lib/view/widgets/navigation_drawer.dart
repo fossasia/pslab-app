16d32210cca0b4a58900b3c43b41a595322b9a60
Marc Nause <marc.nause@audioattack.de>
2026-09-21 22:59:18 +0200

    fix link

--- lib/view/widgets/navigation_drawer.dart
+++ lib/view/widgets/navigation_drawer.dart
@@ -430,7 +430,7 @@ class _NavDrawerState extends State<NavDrawer> {
                 SharePlus.instance.share(
                   ShareParams(
                     text:
-                        '${appLocalizations.shareApp}\nhttps://docs.pslab.io/application/Read/me.html',
+                        '${appLocalizations.shareApp}\nhttps://docs.pslab.io/application/Readme/',
                   ),
                 );
               },
